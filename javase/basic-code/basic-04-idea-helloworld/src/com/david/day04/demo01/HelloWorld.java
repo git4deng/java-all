1c252b7b7d81d01e178dd9b123c8c8316ec72a47
yuanwei <wei.yuan@gh-ca.com>
2019-04-07 21:17:05 +0800

    idea工具的helloworld

---
+++ javase/basic-code/basic-04-idea-helloworld/src/com/david/day04/demo01/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.david.day04.demo01;
+
+/**
+ * @author david
+ * @create 2019-04-07 20:52
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello,world!");
+//        System.out.println("hello,world!");
+//        System.out.println("hello,world!");
+//        System.out.println("hello,world!");
+    }
+}

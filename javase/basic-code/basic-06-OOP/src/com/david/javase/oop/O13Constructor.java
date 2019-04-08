3956ff44f599f2261f7b6976f62cd1958a6c3b02
yuanwei <wei.yuan@gh-ca.com>
2019-04-08 23:15:12 +0800

    构造器

---
+++ javase/basic-code/basic-06-OOP/src/com/david/javase/oop/O13Constructor.java
@@ -0,0 +1,67 @@
+package com.david.javase.oop;
+
+/**
+ * 构造方法是专门用来创建对象的方法，当我们通过关键字new来创建对象时，其实就是在调用构造方法。
+ 格式：
+ public 类名称(参数类型 参数名称) {
+ 方法体
+ }
+
+ 注意事项：
+ 1. 构造方法的名称必须和所在的类名称完全一样，就连大小写也要一样
+ 2. 构造方法不要写返回值类型，连void都不写
+ 3. 构造方法不能return一个具体的返回值
+ 4. 如果没有编写任何构造方法，那么编译器将会默认赠送一个构造方法，没有参数、方法体什么事情都不做。
+ public Student() {}
+ 5. 一旦编写了至少一个构造方法，那么编译器将不再赠送。
+ 6. 构造方法也是可以进行重载的。
+ 重载：方法名称相同，参数列表不同。
+ * @author david
+ * @create 2019-04-08 22:58
+ */
+public class O13Constructor {
+    public static void main(String[] args) {
+        StudentA stu1 = new StudentA(); // 无参构造
+        System.out.println("============");
+
+        StudentA stu2 = new StudentA("赵丽颖", 20); // 全参构造
+        System.out.println("姓名：" + stu2.getName() + "，年龄：" + stu2.getAge());
+        // 如果需要改变对象当中的成员变量数据内容，仍然还需要使用setXxx方法
+        stu2.setAge(21); // 改变年龄
+        System.out.println("姓名：" + stu2.getName() + "，年龄：" + stu2.getAge());
+    }
+}
+class StudentA{
+    // 成员变量
+    private String name;
+    private int age;
+
+    // 无参数的构造方法
+    public StudentA() {
+        System.out.println("无参构造方法执行啦！");
+    }
+
+    // 全参数的构造方法
+    public StudentA(String name, int age) {
+        System.out.println("全参构造方法执行啦！");
+        this.name = name;
+        this.age = age;
+    }
+
+    // Getter Setter
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}

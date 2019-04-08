edec6b29819c045d7e3bcf75bbf8b777b634c245
yuanwei <wei.yuan@gh-ca.com>
2019-04-08 23:15:56 +0800

    重命名类名

---
+++ javase/basic-code/basic-06-OOP/src/com/david/javase/oop/O9EncapsulationMethod.java
@@ -0,0 +1,52 @@
+package com.david.javase.oop;
+
+/**
+ * 封装之private关键字
+ * @author david
+ * @create 2019-04-08 22:31
+ */
+public class O9EncapsulationMethod {
+    public static void main(String[] args) {
+        Person person = new Person();
+        person.show();
+
+        person.name = "赵丽颖";
+//        person.age = -20; // 直接访问private内容，错误写法！
+        person.setAge(20);
+        person.show();
+    }
+}
+/**
+    问题描述：定义Person的年龄时，无法阻止不合理的数值被设置进来。
+    解决方案：用private关键字将需要保护的成员变量进行修饰。
+
+    一旦使用了private进行修饰，那么本类当中仍然可以随意访问。
+    但是！超出了本类范围之外就不能再直接访问了。
+
+    间接访问private成员变量，就是定义一对儿Getter/Setter方法
+
+    必须叫setXxx或者是getXxx命名规则。
+    对于Getter来说，不能有参数，返回值类型和成员变量对应；
+    对于Setter来说，不能有返回值，参数类型和成员变量对应。
+ */
+class Person{
+    String name;
+    private int age;
+    public void show() {
+        System.out.println("我叫：" + name + "，年龄：" + age);
+    }
+
+    // 这个成员方法，专门用于向age设置数据
+    public void setAge(int num) {
+        if (num < 100 && num >= 9) { // 如果是合理情况
+            age = num;
+        } else {
+            System.out.println("数据不合理！");
+        }
+    }
+
+    // 这个成员方法，专门私语获取age的数据
+    public int getAge() {
+        return age;
+    }
+}

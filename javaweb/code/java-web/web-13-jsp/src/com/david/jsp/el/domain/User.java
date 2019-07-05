f15d65151ddf2be26df97cae2b63f0c3e31c2e2f
yuanwei <wei.yuan@gh-ca.com>
2019-07-05 11:00:55 +0800

    jsp

---
+++ javaweb/code/java-web/web-13-jsp/src/com/david/jsp/el/domain/User.java
@@ -0,0 +1,57 @@
+package com.david.jsp.el.domain;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * @author david
+ * @create 2019-06-01 0:08
+ */
+public class User {
+    private String name;
+    private int age;
+    private Date birthday;
+
+    public User(String name, int age, Date birthday) {
+        this.name = name;
+        this.age = age;
+        this.birthday = birthday;
+    }
+
+    public User() {
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getBirStr(){
+       if(birthday!=null){
+           SimpleDateFormat sdf=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+           return sdf.format(birthday);
+       }
+
+        return null;
+    }
+}
--- javaweb/code/java-web/web-13-jsp/web/index.jsp
+++ javaweb/code/java-web/web-13-jsp/web/index.jsp
@@ -1,19 +1,24 @@
-<%--
-  Created by IntelliJ IDEA.
-  User: ghca
-  Date: 2019/5/30
-  Time: 21:31
-  To change this template use File | Settings | File Templates.
---%>
-<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ page import="java.util.ArrayList" %>
+<%@ page import="java.util.List" %>
+<%@ page contentType="text/html;charset=gbk" errorPage="500.jsp"   pageEncoding="GBK" language="java" buffer="16kb" %>
+
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+
 <html>
   <head>
-    <title>index</title>
+    <title>$Title$</title>
   </head>
   <body>
-    <%
-      System.out.println("console!!");
-    %>
-    <h1>首页</h1>
+
+  <%
+
+    List list = new ArrayList();
+    int i = 3/0;
+
+  %>
+
+
+
+
   </body>
 </html>
